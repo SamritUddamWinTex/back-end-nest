e4f43393885702e2d3cbe38adceb09f357553c51
UddamSamrit <samrituddam20@gmail.com>
2024-11-11 18:41:07 +0700

    remove nodemodule

--- dist/task/entities/task.entity.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TaskSchema = void 0;
-const mongoose_1 = require("mongoose");
-exports.TaskSchema = new mongoose_1.Schema({
-    title: String,
-});
-//# sourceMappingURL=task.entity.js.map
